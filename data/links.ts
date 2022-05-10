f16dc00467746aaf1707c7adf519c92d9868aa45
Santos Garcia <89603557+JAIM307@users.noreply.github.com>
2022-05-10 16:29:56 -0500

    Fixed IDE Saving

--- data/links.ts
+++ data/links.ts
@@ -7,43 +7,43 @@ export const Links = [
     },
     {
         id: 2,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/a64a7fb24d',
         link:'https://www.youtube.com/embed/JOQpuR-3GMQ',
         name:'Lesson 2: Working with data'
     },
     {
         id: 3,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/7d6da10fe6',
         link:'https://www.youtube.com/embed/5fDdWR4e80k',
         name:'Lesson 3: Input and output'
     },
     {
         id: 4,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/f6ca81708d',
         link:'https://www.youtube.com/embed/OaK24pcfNZs',
         name:'Lesson 4: Making decisions'
     },
     {
         id: 5,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/01c51f4421',
         link:'https://www.youtube.com/embed/PA3kicE2I7g',
         name:'Lesson 5: Lists and loops'
     },
     {
         id: 6,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/212e2d623c',
         link:'https://www.youtube.com/embed/oqMZJALlOko',
         name:'Lesson 6: Numeric and date functions'
     },
     {
         id: 7,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/123d3dc2d4',
         link:'https://www.youtube.com/embed/gEAhVKKm7WA',
         name:'Lesson 7: Functions'
     },
     {
         id: 8,
-        python: 'https://trinket.io/embed/python/298de446f7',
+        python: 'https://trinket.io/embed/python/ab19cee4a3',
         link:'https://www.youtube.com/embed/4AEZNAFoxfU',
         name:'Lesson 8: Python classes'
     },
